a65515fe7246bc6490ea9b7b38bd24a296d7ee06
littlechicken2 <jint8@mcmaster.ca>
2019-04-07 14:33:03 -0400

    Measure
    add measure methods

--- src/Intersection.java
+++ src/Intersection.java
@@ -22,7 +22,17 @@ public class Intersection {
 		double b = this.lon - x.lon;
 		return Math.sqrt(a*a + b*b);
 	}
-	
+	public double measure(Intersection x){
+	    double R = 6378.137; // Radius of earth in KM
+	    double dLat = Math.abs(this.lat * Math.PI / 180 - x.lat * Math.PI / 180);
+	    double dLon = Math.abs(this.lon * Math.PI / 180 - x.lon * Math.PI / 180);
+	    double a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+	    Math.cos(x.lat * Math.PI / 180) * Math.cos(this.lat * Math.PI / 180) *
+	    Math.sin(dLon/2) * Math.sin(dLon/2);
+	    double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+	    double d = R * c;
+	    return d * 1000; // meters
+	}
 	public String toString(){
 		return "Latitude: " + this.lat + ", Longitude: " + this.lon;
 	}
