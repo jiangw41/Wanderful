2e88f691217cea6303e8b7577a0f4144600b57bf
Andy Ng <Pikappy@DESKTOP-DJTGUOQ.localdomain>
2019-04-10 16:41:22 -0400

    Push

--- src/Reader.java
+++ src/Reader.java
@@ -7,33 +7,44 @@ import java.util.Scanner;
 public class Reader {
 	
 	// Reads dataset
-	public static Intersection[] Hamilton(){
-		int recommendedSteps = 10000;
-		Intersection[] intersection = new Intersection[252767];
-		int i = 0;		// i refers to the ObjectID in dataset
+	public static Intersection[] Hamilton(int currentMeters){
+		int recommendedMeters = 9000;
+		int reqMeters = recommendedMeters - currentMeters;
+		Intersection[] intersection = new Intersection[252766];
+		int i = 0;
+		Intersection currentLocation = new Intersection(-79.918862, 43.258470);
+		
 		try {
 			Scanner input = new Scanner(new File("src/HamiltonGIS.csv"));
+			String current = input.nextLine();
 			while (input.hasNextLine()){
-				String current = input.nextLine();
+				current = input.nextLine();
 				String[] list = current.split(",");						// Splits line by comma
-				if (i != 0){
-					intersection[i] = new Intersection(Double.parseDouble(list[3]), Double.parseDouble(list[4]));
+				intersection[i] = new Intersection(Double.parseDouble(list[3]), Double.parseDouble(list[4]));
+				if (true){	// if currentlocation to points in dataset is within reqMeters
+					i++;	// saves this as intersection[i] and looks for next one
 				}
-				/* if longitude and latitude is within the current location's radius
-				Location[0] = Double.parseDouble(list[3]);			// Longitude of city
-				Location[1] = Double.parseDouble(list[4]);			// Latitude of city
-				*/
-				
-				i++;
+				//System.out.println(currentLocation.distTo(intersection[i]));
 			}
 			input.close();
+			return intersection;
 		}
 		catch (FileNotFoundException e){
 			System.out.println("File could not be found");
 		}
 		return intersection;
 	}
-	
+
 	// Reads a certain area (fixed radius) around user's current location
 	// Adds nodes inside area onto another text file - connect all nodes together
+	
+	// Obtains points from dataset within a radius of user's recommended number of steps left
+	// Connects the points to a graph
+	public static void region(Intersection[] x){
+		for(int i = 0; i<x.length; i++){
+			System.out.println(x[i]);
+		}
+		System.out.println(x.length);
+	}
+	
 }
